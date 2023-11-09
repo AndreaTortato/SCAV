c515e33cee75bfdc3688dc7e1fa4d3ea524caf5f
AndreaTortato <atortato@tradeheader.com>
2023-11-09 19:32:53 +0100

    Exercise 5

--- P2/exercises.py
+++ P2/exercises.py
@@ -114,3 +114,70 @@ def read_video_info(input_file):
 
 read_video_info("bunny.mp4")
 
+
+#Exercise 5
+#Use function from P1 to transform each frame of the video in black and white.
+#First we obtain a frame, we tranform it to BnW and then we add them up again
+#(not working for now)
+
+import subprocess
+import sys
+sys.path.insert(1, '../P1/exercise4.py')
+from exercise4 import transform_to_BnW
+
+def make_video_BnW(input_file, output_file):
+    try:
+        temp_frames_txt = 'temp_frames.txt'
+
+        # Create a text file to store the list of transformed frames
+        with open(temp_frames_txt, 'w') as file:
+            frame_number = 0
+
+            while True:
+                input_frame = f'temp_frame_{frame_number}.jpg'
+                output_frame = f'temp_frame_BnW_{frame_number}.jpg'
+
+                # Extract the current frame
+                extract_frame_cmd = [
+                    'ffmpeg',
+                    '-i', input_file,
+                    '-vf', f'select=gte(n\,{frame_number})',
+                    '-vframes', '1',
+                    input_frame
+                ]
+                subprocess.run(extract_frame_cmd, check=True)
+
+                # Transform the current frame
+                transform_to_BnW(input_frame, output_frame, quality=2)
+
+                # Write the path of the transformed frame to the text file
+                file.write(f"file '{output_frame}'\n")
+
+                frame_number += 1
+
+                # Break the loop if there are no more frames
+                if not subprocess.run(['ffmpeg', '-i', input_file], stdout=subprocess.PIPE,
+                                      stderr=subprocess.PIPE).returncode == 0:
+                    break
+
+        # Combine the transformed frames into the output video
+        combine_cmd = [
+            'ffmpeg',
+            '-f', 'concat',
+            '-safe', '0',
+            '-i', temp_frames_txt,
+            '-c', 'h264',
+            '-pix_fmt', 'yuv420p',
+            output_file
+        ]
+        subprocess.run(combine_cmd, check=True)
+
+        print(f"Black and white video created: {output_file}")
+
+    except subprocess.CalledProcessError as e:
+        print(f"FFmpeg error: {e.output}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+make_video_BnW("bunny.mp4", "bunny_BnW.mp4")
