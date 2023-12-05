61e5eab373e7eb478cf0175babbaac79acc0e2ee
AndreaTortato <atortato@tradeheader.com>
2023-12-05 16:53:55 +0100

    Added options

--- SP3/exercise3.py
+++ SP3/exercise3.py
@@ -3,6 +3,7 @@ from tkinter import filedialog
 from tkVideoPlayer.tkvideoplayer import TkinterVideo # pip install tkVideoPlayer, to play the video in the app
 import os
 import time
+import subprocess
 
 import sys
 sys.path.insert(1, 'exercise1.py')
@@ -19,6 +20,9 @@ class VideoConverterApp:
         master.geometry("848x480")
         root.configure(bg="#ffcc00")
 
+        # Get the repository root path
+        self.repo_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
         self.input_video_path = tk.StringVar()
         self.target_resolution = tk.StringVar()
         self.output_format = tk.StringVar()
@@ -32,7 +36,8 @@ class VideoConverterApp:
         # play/pause button
         self.play_pause_button_text = tk.StringVar()
         self.play_pause_button_text.set("Pause")
-        self.play_pause_button = tk.Button(self.master, textvariable=self.play_pause_button_text, command=self.toggle_play_pause)
+        self.play_pause_button = tk.Button(self.master, textvariable=self.play_pause_button_text,
+                                           command=self.toggle_play_pause)
         self.play_pause_button.pack(side=tk.BOTTOM, anchor=tk.SW, padx=10, pady=10)
 
         # Video Player
@@ -40,14 +45,16 @@ class VideoConverterApp:
         self.video_player.pack(side=tk.BOTTOM, expand=True, fill="both", padx=10, pady=10)
 
         # Other options
-        actions = ["Options", "Compare Videos", "Return motion vectors", "Convert to B&W", "convert to MP2", "Change Chroma SubSamp."]
+        actions = ["Options", "Return motion vectors", "Convert to B&W", "Convert to MP2", "Change Chroma SubSamp."]
+        self.option_button = tk.Button(self.master, text="Accept option", command=self.select_option)
+        self.option_button.pack(side=tk.BOTTOM, anchor=tk.W, padx=10, pady=10)
         self.option_dropdown = tk.OptionMenu(self.master, self.selected_option, *actions)
         self.selected_option.set(actions[0])
         self.option_dropdown.pack(side=tk.BOTTOM, anchor=tk.W, padx=10, pady=10)
 
         # Select Video Button
         self.select_video_button = tk.Button(self.master, text="Select Video", command=self.select_video)
-        self.select_video_button.pack(side=tk.LEFT, anchor=tk.NW, padx=10, pady=10) #to position the widgets
+        self.select_video_button.pack(side=tk.LEFT, anchor=tk.W, padx=10, pady=10)  # to position the widgets
 
         # Target Resolution Dropdown
         resolutions = ["720p", "480p", "360x240", "160x120"]
@@ -65,19 +72,20 @@ class VideoConverterApp:
         self.output_format.set(formats[0])
         self.format_dropdown.pack(side=tk.LEFT, anchor=tk.NW, padx=0, pady=8)
 
-        #Convert video
+        # Convert video
         self.convert_button = tk.Button(self.master, text="Convert video", command=self.convert_video)
         self.convert_button.pack(side=tk.RIGHT, anchor=tk.NE, padx=10, pady=10)
-
 
     def select_video(self):
         file_path = filedialog.askopenfilename(filetypes=[("Video files", "*.mp4;*.mkv")])
         if file_path:
-            self.input_video_path.set(file_path)
-            print(f"Selected video: {file_path}")
 
-            # Set window size based on video size
-            self.master.geometry(f"{848}x{480}")
+            # Get the relative path from the repository root
+            relative_path = os.path.relpath(file_path, start=self.repo_root)
+
+            # Set the relative path to self.input_video_path
+            self.input_video_path.set(relative_path)
+            print(f"Selected video: {relative_path}")
 
             self.video_player.load(file_path)
             self.video_player.play()
@@ -87,14 +95,19 @@ class VideoConverterApp:
         option = self.selected_option.get()
         input_video_path = self.input_video_path.get()
 
+        print(input_video_path)
+
         if option == "Return motion vectors":
             visualize_motion_vectors(input_video_path, "MV_"+input_video_path)
+
         elif option == "Convert to B&W":
             make_video_BnW(input_video_path, "B&W_"+input_video_path)
+
         elif option == "Convert to MP2":
             convert_to_mp2(input_video_path, "mp2"+input_video_path)
+
         elif option == "Change Chroma SubSamp":
-            change_chroma_subsampling(input_video_path, "new_ChromaSS_"+input_video_path)
+            change_chroma_subsampling(input_video_path, "new_ChromaSS_"+input_video_path, "yuv420p")
 
     def toggle_play_pause(self):
         if self.video_player.is_paused():
